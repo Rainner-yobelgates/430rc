044644f6095fea94dcfb803bbe845f34ecfbbcdf
ronaregen <ronaregen@gmail.com>
2025-02-16 11:50:47 +0700

    fix/seeder: coment json_decode api rajaongkir

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,19 +22,19 @@ class DatabaseSeeder extends Seeder
             'email' => '430rc@gmail.com',
             'password' => bcrypt('adminfor430rc'),
         ]);
-        foreach (json_decode(get_api_province())->rajaongkir->results as $province) {
-            Province::create([
-                'province' => $province->province
-            ]);
-        }
-        foreach (json_decode(get_api_city())->rajaongkir->results as $city) {
-            City::create([
-                'province_id' => $city->province_id,
-                'type' => $city->type,
-                'city' => $city->city_name,
-                'postal_code' => $city->postal_code
-            ]);
-        }
+        // foreach (json_decode(get_api_province())->rajaongkir->results as $province) {
+        //     Province::create([
+        //         'province' => $province->province
+        //     ]);
+        // }
+        // foreach (json_decode(get_api_city())->rajaongkir->results as $city) {
+        //     City::create([
+        //         'province_id' => $city->province_id,
+        //         'type' => $city->type,
+        //         'city' => $city->city_name,
+        //         'postal_code' => $city->postal_code
+        //     ]);
+        // }
 
     }
 }
