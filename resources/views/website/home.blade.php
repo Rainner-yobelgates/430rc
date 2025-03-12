49baa5474729ea45ce7684258ce140560918eb70
Rainner Yobelgates <ryfranceisten@gmail.com>
2025-03-13 01:05:36 +0700

    update font

--- resources/views/website/home.blade.php
+++ resources/views/website/home.blade.php
@@ -68,7 +68,7 @@
                 @foreach ($getFaq as $key => $faq)
                     <div class="accordion-item">
                     <h2 class="accordion-header" id="heading{{$key}}">
-                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse{{$key}}" aria-expanded="false" aria-controls="collapse{{$key}}">
+                        <button class="accordion-button collapsed fw-bold" type="button" data-bs-toggle="collapse" data-bs-target="#collapse{{$key}}" aria-expanded="false" aria-controls="collapse{{$key}}">
                             {{$faq->title}}
                         </button>
                     </h2>
